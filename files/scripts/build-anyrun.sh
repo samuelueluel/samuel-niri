50b80df52311448df3f2eb6e648213eaab681e88
samuelueluel <s.a.saltmarsh@gmail.com>
2026-04-05 19:41:23 -0600

    anyrun build script fix

--- files/scripts/build-anyrun.sh
+++ files/scripts/build-anyrun.sh
@@ -5,6 +5,7 @@ set -euo pipefail
 
 VERSION="v25.12.0"
 REPO_URL="https://github.com/anyrun-org/anyrun.git"
+PROVIDER_URL="https://github.com/anyrun-org/anyrun-provider.git"
 BUILD_DIR="$(mktemp -d)"
 trap "rm -rf '$BUILD_DIR'" EXIT
 
@@ -20,16 +21,21 @@ dnf install -y --setopt=install_weak_deps=False \
     git
 
 git clone --depth=1 --branch "$VERSION" "$REPO_URL" "$BUILD_DIR/src"
+git clone --depth=1 --branch "$VERSION" "$PROVIDER_URL" "$BUILD_DIR/provider"
 
 cd "$BUILD_DIR/src"
 cargo build --release
 
+cd "$BUILD_DIR/provider"
+cargo build --release
+
 # Install binary
-install -Dm755 target/release/anyrun /usr/bin/anyrun
+install -Dm755 "$BUILD_DIR/src/target/release/anyrun" /usr/bin/anyrun
+install -Dm755 "$BUILD_DIR/provider/target/release/anyrun-provider" /usr/bin/anyrun-provider
 
 # Install plugins
 mkdir -p /usr/lib64/anyrun/plugins
-install -Dm755 target/release/lib*.so -t /usr/lib64/anyrun/plugins/
+install -Dm755 "$BUILD_DIR/src/target/release"/lib*.so -t /usr/lib64/anyrun/plugins/
 
 # Remove build toolchain
 dnf remove -y cargo rust gtk4-devel gtk4-layer-shell-devel pango-devel cairo-devel gdk-pixbuf2-devel glib2-devel
