6ff7220eb0795450291e74f3e587175da351af94
Samuel Saltmarsh <s.a.saltmarsh@gmail.com>
2026-08-25 17:39:50 -0600

    update

--- files/system/usr/share/turquoise/llama-dispatch-wrapper.sh
+++ files/system/usr/share/turquoise/llama-dispatch-wrapper.sh
@@ -4,13 +4,17 @@
 # All models default to Nathanw1014 Vulkan RADV engine (strix-halo-vulkan)
 
 USE_ROCM=0
+USE_LAURENT=0
 CLEAN_CMD=()
 
 for arg in "$@"; do
   if [[ "$arg" == *"--force-rocm"* ]]; then
     USE_ROCM=1
+  elif [[ "$arg" == *"--force-laurent"* ]]; then
+    USE_LAURENT=1
   elif [[ "$arg" == *"--force-vulkan"* ]]; then
     USE_ROCM=0
+    USE_LAURENT=0
   else
     CLEAN_CMD+=("$arg")
   fi
@@ -26,11 +30,22 @@ if [[ $USE_ROCM -eq 1 ]]; then
     export LD_LIBRARY_PATH="/opt/lemonade/llama/rocm/lib:/opt/lemonade/llama/rocm:/opt/rocm/lib:/opt/rocm/lib/llvm/lib:$LD_LIBRARY_PATH"
     exec /opt/lemonade/llama/rocm/llama-server "${CLEAN_CMD[@]}"
   fi
+elif [[ $USE_LAURENT -eq 1 ]]; then
+  # Laurent Zuijdwijk Vulkan RADV engine (Adaptive DFlash2 + ROCmFP4)
+  export VK_ICD_FILENAMES="/opt/lemonade/llama/vulkan-laurent/driver/radeon_icd.x86_64.json"
+  export VK_DRIVER_FILES="$VK_ICD_FILENAMES"
+  export LD_LIBRARY_PATH="/opt/lemonade/llama/vulkan-laurent/bin:/opt/lemonade/llama/vulkan-laurent/driver"
+  export GGML_VK_MMID_ROWLISTS=1 GGML_VK_MMID_SMALLN=1 GGML_VK_MMID_BM64=1 GGML_VK_MMID_WAVE32=1 GGML_VK_MMID_F16B=1 GGML_VK_MMID_M128=1 GGML_VK_FA_WAVE32=1 GGML_VK_FA_DEQUANT=1 GGML_VK_MAX_NODES_PER_SUBMIT=64
+  exec /opt/lemonade/llama/vulkan-laurent/bin/llama-server "${CLEAN_CMD[@]}"
 else
   # Nathanw1014 Vulkan RADV engine (strix-halo-vulkan)
   export VK_ICD_FILENAMES="/opt/lemonade/llama/vulkan/driver/radeon_icd.x86_64.json"
   export VK_DRIVER_FILES="$VK_ICD_FILENAMES"
   export LD_LIBRARY_PATH="/opt/lemonade/llama/vulkan/bin:/opt/lemonade/llama/vulkan/driver"
   export GGML_VK_MMID_ROWLISTS=1 GGML_VK_MMID_SMALLN=1 GGML_VK_MMID_BM64=1 GGML_VK_MMID_WAVE32=1 GGML_VK_MMID_F16B=1 GGML_VK_MMID_M128=1 GGML_VK_FA_WAVE32=1 GGML_VK_FA_DEQUANT=1 GGML_VK_MAX_NODES_PER_SUBMIT=64
-  exec /opt/lemonade/llama/vulkan/bin/llama-server "${CLEAN_CMD[@]}"
+  if [[ -f /opt/lemonade/llama/vulkan/bin/llama-server-real ]]; then
+    exec /opt/lemonade/llama/vulkan/bin/llama-server-real "${CLEAN_CMD[@]}"
+  else
+    exec /opt/lemonade/llama/vulkan/bin/llama-server "${CLEAN_CMD[@]}"
+  fi
 fi
